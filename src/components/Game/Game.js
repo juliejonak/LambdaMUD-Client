57078e9a66c80f0ba1fb36783bd052608d0fb06b
Ben <cbtsao47@gmail.com>
2019-07-31 01:20:22 -0500

    game component now initializes user and subscribe to a pusher channel on mount. handleMovement method added for direction control

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -5,20 +5,63 @@ import InputBox from "../InputBox/InputBox";
 import GlobalNotification from "../GlobalNotification/GlobalNotification";
 import config from "../../config";
 import Pusher from "pusher-js";
-import axios from "axios";
 
 // rendered by App
 export default class Game extends Component {
+  state = {
+    uuid: "",
+    name: "",
+    title: "",
+    description: "",
+    players: [],
+    notifications: "",
+    userInput: "",
+    error_msg: ""
+  };
+  // TO DO,
+  handleChange = e => {
+    this.setState({ userInput: e.target.value });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    const { userInput } = this.state;
+    config
+      .axiosWithAuth("/api/adv/say/", userInput)
+      .then(({ data }) => {
+        // TO DO,
+        // find out what we intend to do after a user has submit an input
+      })
+      .catch(err => console.log(err));
+  };
+  /**
+   * Updates the user's character placement in the game,
+   * @param: direction,, string, can only be n,e,w,s
+   */
+  handleMovement = direction => {
+    config
+      .axiosWithAuth()
+      .post("/api/adv/move/", { direction })
+      .then(({ data: { title, description, players, error_msg } }) => {
+        this.setState({
+          title,
+          description,
+          players: [...this.state.players, ...players],
+          error_msg
+        });
+      })
+      .catch(err => console.log(err));
+  };
+
   /**
    * Initializes the user's character into the game,
-   *@param: key, the authToken required to initialize the user, received from login/register endpoints
+   *@param: none
    */
-  initializeGame = key => {
-    axios
-      .get(`${config.apiUrl}/api/adv/init/`, {
-        headers: { Authorization: `Token ${key}` }
-      })
+  initializeGame = () => {
+    config
+      .axiosWithAuth()
+      .get(`/api/adv/init/`)
       .then(({ data: { uuid, name, title, description, players } }) => {
+        this.setState({ uuid, name, title, description, players });
         this.subscribeToChannel(uuid);
       })
       .catch(err => {
@@ -34,26 +77,32 @@ export default class Game extends Component {
       cluster: process.env.REACT_APP_PUSHER_CLUSTER
     });
     const channel = pusher.subscribe(`p-channel-${uuid}`);
-    console.log(channel);
+    /*
+    binds to broadcast event to the channel, what this actually does is just
+    basically adding an ongoing event listener, setState gets called whenever
+    the event gets triggered in the back end.
+    */
     channel.bind("broadcast", data => {
-      console.log("broadcast response", data.message);
-      //     this.setState({
-
-      //   });
+      this.setState({
+        notifications: data.message
+      });
     });
   };
   componentDidMount() {
-    const key = localStorage.getItem("authToken");
-    this.initializeGame(key);
+    this.initializeGame();
   }
 
   render() {
+    const { notifications } = this.state;
     return (
       <div>
         <Map />
-        <Directions />
-        <InputBox />
-        <GlobalNotification />
+        <Directions handleMovement={this.handleMovement} />
+        <InputBox
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
+        <GlobalNotification notifications={notifications} />
       </div>
     );
   }
