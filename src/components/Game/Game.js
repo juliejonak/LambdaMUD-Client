5cabb142647aa3a40f787e7a728b1e950659af43
Ben <cbtsao47@gmail.com>
2019-07-31 00:14:14 -0500

    add initializer and subscribe when component is mounted

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,11 +3,49 @@ import Map from "../Map/Map";
 import Directions from "../Directions/Directions";
 import InputBox from "../InputBox/InputBox";
 import GlobalNotification from "../GlobalNotification/GlobalNotification";
+import config from "../../config";
+import Pusher from "pusher-js";
 import axios from "axios";
 
 // rendered by App
 export default class Game extends Component {
-  componentDidMount() {}
+  /**
+   * Initializes the user's character into the game,
+   *@param: key, the authToken required to initialize the user, received from login/register endpoints
+   */
+  initializeGame = key => {
+    axios
+      .get(`${config.apiUrl}/api/adv/init/`, {
+        headers: { Authorization: `Token ${key}` }
+      })
+      .then(({ data: { uuid, name, title, description, players } }) => {
+        this.subscribeToChannel(uuid);
+      })
+      .catch(err => {
+        console.log(err.response.data);
+      });
+  };
+  /**
+   * subscribeToChannel subscribes to a pusher channel and bind to broadcast events
+   *@param: uuid, subscribe to the uuid channel provided
+   */
+  subscribeToChannel = uuid => {
+    const pusher = new Pusher(process.env.REACT_APP_PUSHER_API_KEY, {
+      cluster: process.env.REACT_APP_PUSHER_CLUSTER
+    });
+    const channel = pusher.subscribe(`p-channel-${uuid}`);
+    console.log(channel);
+    channel.bind("broadcast", data => {
+      console.log("broadcast response", data.message);
+      //     this.setState({
+
+      //   });
+    });
+  };
+  componentDidMount() {
+    const key = localStorage.getItem("authToken");
+    this.initializeGame(key);
+  }
 
   render() {
     return (
