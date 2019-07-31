85fd9e221bab51c2098362a66d3727cf56139aa7
Ben <cbtsao47@gmail.com>
2019-07-31 01:01:52 -0500

    add axiosWithAuth helper

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,20 @@
+import axios from "axios";
 /**
  * Creates a config object with commonly used URLs or keys throughout the application
  * Allows for single spot updating of frequently used information across components
+ * axiosWithAuth returns an axios instance with the authorization headers and baseURL
  */
 export const config = {
-  // apiUrl: "http://127.0.0.1:8000"
-  apiUrl: "https://lambda-mud-test.herokuapp.com"
+  apiUrl: "http://127.0.0.1:8000",
+  axiosWithAuth: function() {
+    return axios.create({
+      baseURL: this.apiUrl,
+      headers: {
+        Authorization: `Token ${localStorage.getItem("authToken")}`
+      }
+    });
+  }
+  //   apiUrl: "https://lambda-mud-test.herokuapp.com"
 };
 
 export default config;
