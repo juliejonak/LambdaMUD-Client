8bf10887bb1ca599bc0bf9ff7bad6b5c6a3174f2
Ben <cbtsao47@gmail.com>
2019-07-31 21:58:53 -0500

    add backend deployed url

--- src/config/index.js
+++ src/config/index.js
@@ -7,6 +7,7 @@ import axios from "axios";
 export const config = {
   // apiUrl: "http://127.0.0.1:8000",
   apiUrl: "https://lambda-mud-test.herokuapp.com",
+  // apiUrl: "https://cs-mud-backend.herokuapp.com",
   /**
    * @returns an Authorization Header with the user's authToken
    */
